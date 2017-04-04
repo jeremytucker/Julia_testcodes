f671460bdedcd67c03908ddba7fc83531304929f
Jose Daniel Lara <jdlara@berkeley.edy>
2017-04-04 13:13:15 -0700

    error fixes

--- Savio/compilation_test.jl
+++ Savio/compilation_test.jl
@@ -1,4 +1,4 @@
 using BuildExecutable
 using Base.Test
 using Compat
-build_executable(test_exec, test_code, exec_folder, "native")
+build_executable("test_exec", "test_code.jl", "exec_folder", "native")
