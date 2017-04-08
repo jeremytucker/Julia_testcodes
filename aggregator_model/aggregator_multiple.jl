65c971bfc121edf38dad9707caeafc5b6b952852
Jose Daniel Lara <jdlara@berkeley.edy>
2017-04-08 13:21:50 -0700

    started the multiple fleet aggregator, wip.

--- aggregator_model/aggregator_multiple.jl
+++ aggregator_model/aggregator_multiple.jl
@@ -12,15 +12,6 @@ if is_apple()
     using Gurobi
 end    
 
-
-#data 
-time = 8750 #[hrs]
-data_p = readtable("CA_Weighted_Price_Case0.csv");
-data_c = readtable("car_profile.csv");
-ini = 1;
-prices = convert(Array,data_p[ini:(ini+time-1),end]);
-car = convert(Array,data_c);
-
 # JuMP model 
 if is_linux()
     agg = Model(solver=CplexSolver(CPX_PARAM_EPGAP=1e-04))
@@ -30,28 +21,41 @@ if is_apple()
     agg = Model(solver=GurobiSolver(Presolve=1, InfUnbdInfo=1))
 end  
 
+#data 
+time = 8750 #[hrs]
+data_p = readtable("CA_Weighted_Price_Case0.csv");
+data_c = readtable("car_profile.csv");
+ini = 1;
+prices = convert(Array,data_p[ini:(ini+time-1),end]);
+car = convert(Array,data_c);
+
+
 
 #Parameters
+
+
 availability = car #[0-1]
+k_SOC = availability #[0-1] 
+k_power = availability #[0-1]
+
 contract_limit = 0.8 #[%]
-fleet = 1000000 #[units]
+fleet_no = 1 #[units]
 bat_size = 30 #[kWh], based on the Nissan Leaf 
 charger_power = 7.2 #[kW], based on a 30 Amp Charger
 eff_in = 0.85 #[%]
 eff_out = 0.78 #[%]
-k_power = availability #[0-1]
-P_max_in = fleet*charger_power #[kW]
-P_max_out = fleet*charger_power #[kW]
-SOC_max = fleet*bat_size #[kWh]
-SOC_min = fleet*bat_size*(1-contract_limit) #[kWh]
-SOC_ini = SOC_max #[kWh] 
-k_SOC = availability #[0-1] 
+P_max_in[1:fleet] = fleet*charger_power #[kW]
+P_max_out[1:fleet] = fleet*charger_power #[kW]
+SOC_max[1:fleet] = bat_size #[kWh]
+SOC_min[1:fleet] = bat_size*(1-contract_limit) #[kWh]
+SOC_ini[1:fleet] = SOC_max #[kWh] 
+
 
 @variables agg begin 
-    P_in[t=1:time] >= 0
-    P_out[t=1:time] >= 0
-    SOC_min*k_SOC[t] <= SOC[t = 1:time] <= SOC_max*k_SOC[t]
-    control[t=1:time], Bin
+    P_in[f = 1:fleet, t = 1:time] >= 0
+    P_out[f = 1:fleet, t = 1:time] >= 0
+    SOC_min*k_SOC[f,t] <= SOC[f = 1:fleet, t = 1:time] <= SOC_max*k_SOC[f, t]
+    control[f = 1:fleet, t=1:time], Bin
 end 
 
 @constraints agg begin
