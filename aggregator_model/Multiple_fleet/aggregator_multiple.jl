7d11441721fc0f820364fa52cecc17236d1501f5
jdlara-berkeley <jdlara@ln000.brc>
2017-04-17 00:06:10 -0700

    still fails commercial

--- aggregator_model/Multiple_fleet/aggregator_multiple.jl
+++ aggregator_model/Multiple_fleet/aggregator_multiple.jl
@@ -23,7 +23,7 @@ if is_apple()
 end  
 
 #data 
-time = 2  #[hrs]
+time = 8760  #[hrs]
 data_p = readtable(ARGS[1])
 data_c = readtable("car_profiles/car_profile_commercial.csv")
 ini = 1
@@ -44,7 +44,7 @@ P_max_in = repmat(fleet_size',time,1).*(charger_power*k_power[1:time, 1:fleet_ty
 P_max_out = repmat(fleet_size',time,1).*(charger_power*k_power[1:time, 1:fleet_types]) #[kW]
 SOC_max = bat_size*repmat(fleet_size',time,1).*k_SOC[1:time, 1:fleet_types] #[kWh]
 SOC_min = bat_size*(1-contract_limit)*repmat(fleet_size',time,1).*k_SOC[1:time, 1:fleet_types] #[kWh]
-SOC_ini = fleet_size.*bat_size*0.05 #[kWh] 
+SOC_ini = fleet_size.*bat_size*0.045 #[kWh] 
 
 
 @variables agg begin
@@ -64,7 +64,7 @@ end
 
 @objective(agg, Max, sum(sum(prices[t]/1000*(P_out[t,f] - P_in[t,f]) for t in 1:time) for f in 1:fleet_types));
 
-#writeLP(agg, "lp_file.lp", genericnames=false)
+writeLP(agg, "lp_file.lp", genericnames=false)
 solve(agg)
 
 writedlm("results_multi/P_out_$(ARGS[2])_$(contract_limit*100).txt", getvalue(P_out))
